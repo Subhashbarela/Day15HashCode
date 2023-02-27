71a2cc29ec6e295e68450b0f30e8ccd4aa70be5e
Subhash Barela <barelasubhash16@gmail.com>
2023-02-27 13:35:51 +0530

    occurance of WorldFrequency

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashFunction_in_DS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordFrequency.Frequency();
+        }
+    }
+}
